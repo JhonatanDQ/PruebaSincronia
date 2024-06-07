f19d2b4e440cb9057eee39b1fa117da4857a3c2e
Jhonatan Quiroga <jdquirogape@ufpso.edu.co>
2024-06-07 18:10:08 -0500

    PruebaSincronia

---
+++ src/components/vue/LoadData.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref} from 'vue';
+
+const users = ref([])
+
+function loadData(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+  .then(function(response){ 
+    return response.json()})
+  .then(function (data) { 
+    console.log(data)
+    users.value = data
+})
+  }
+</script>
+
+
+
+<template>
+    <div 
+        v-for="users in users" :key="users.id"
+    class="bg-slate-300 mb-1"> 
+    {{ users.name }}
+</div> 
+    <button @click="loadData">
+        cargar info 
+    </button>
+</template>
